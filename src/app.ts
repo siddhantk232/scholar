1b1c41ae344bc1d71a7c16b626d3a1cbd4bbc8c4
siddhantCodes <siddhantk232@gmail.com>
2022-06-08 14:25:48 +0530

    express server setup

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log("hello world");
+import express from "express";
+
+(function () {
+  const app = express();
+
+  app.get("/", (_, res) => res.send("ping"));
+
+  const server = app.listen(3000, () => console.log("listening"));
+
+  process.once("SIGTERM", handleClose);
+  process.once("SIGINT", handleClose);
+
+  function handleClose() {
+    console.log("closing");
+    server.close();
+  }
+})()
+
