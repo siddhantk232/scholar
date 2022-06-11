558a01985c3666f86b2fb1caec13072e4a45367b
siddhantCodes <siddhantk232@gmail.com>
2022-06-11 19:03:45 +0530

    teacher controller

--- src/components/teacher/controllers/teacherController.ts
+++ src/components/teacher/controllers/teacherController.ts
@@ -1,5 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Role } from "@prisma/client";
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import Joi from "joi";
+
+import {
+  teacherUpdateObject,
+  TeacherUpdatePayload,
+} from "../validators/teacherValidator";
 
 export class TeacherController {
   private db: PrismaClient;
@@ -8,20 +15,74 @@ export class TeacherController {
     this.db = db;
   }
 
-  getTeachers(_: Request, res: Response) {
-    res.send("not implemented");
-    console.log(typeof this.db);
+  async getTeachers(_: Request, res: Response) {
+    const teachers = await this.db.user.findMany({
+      where: { kind: Role.TEACHER },
+    });
+
+    res.status(StatusCodes.OK).json({ ok: true, teachers });
   }
-  getTeacher(_: Request, res: Response) {
-    res.send("not implemented");
+
+  async getTeacher(req: Request, res: Response) {
+    const id = parseInt(req.params.id);
+
+    if (!id) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ ok: false, message: "Invalid id" });
+    }
+
+    const teacher = await this.db.user.findFirst({
+      where: { id, kind: Role.TEACHER },
+    });
+
+    return res.status(StatusCodes.OK).json({ ok: true, teacher });
   }
-  deleteTeacher(_: Request, res: Response) {
-    res.send("not implemented");
+
+  async deleteTeacher(req: Request, res: Response) {
+    const id = parseInt(req.params.id);
+
+    if (!id) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ ok: false, message: "Invalid id" });
+    }
+
+    const teacher = await this.db.user.delete({ where: { id } });
+
+    return res
+      .status(StatusCodes.OK)
+      .json({ ok: true, message: "Teacher deleted successfully", teacher });
   }
-  createTeacher(_: Request, res: Response) {
-    res.send("not implemented");
+
+  async updateTeacher(req: Request, res: Response) {
+    const id = parseInt(req.params.id);
+
+    if (!id) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ ok: false, message: "Invalid id" });
+    }
+
+    const { error, value: data } = this.validate(teacherUpdateObject, req.body);
+
+    if (error) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ ok: false, message: error.message });
+    }
+
+    const teacher = await this.db.user.update({ where: { id }, data });
+
+    return res
+      .status(StatusCodes.OK)
+      .json({ ok: true, message: "Teacher updated successfully", teacher });
   }
-  updateTeacher(_: Request, res: Response) {
-    res.send("not implemented");
+
+  private validate(
+    validator: Joi.ObjectSchema<TeacherUpdatePayload>,
+    payload: any
+  ) {
+    return validator.validate(payload);
   }
 }
